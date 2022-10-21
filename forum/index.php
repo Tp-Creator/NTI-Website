b5809ca3bd16a721759f82f404ccf2a63b33e771
Joel <j.jagerskogh@gmail.com>
2022-10-21 15:16:53 +0200

    Har flyttat och skapat ny fil för att eventuellt kunna ansluta till databas
    idk

--- forum/index.php
+++
@@ -1,3 +0,0 @@
-<?php
-    echo "Welcome to this php test website"
-?>
---
+++ includes/dbh.inc.php
@@ -0,0 +1,12 @@
+<?php
+//Incluces database grejer
+
+$dbServername = "localhost";
+$dbUsername = "root";
+$dbPassword = "";
+$dbName = "NTI-server";
+
+//Creates connection to the MySQL database
+$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
+
+?>
