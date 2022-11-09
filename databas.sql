4ba2bf4e756fb4c7eb75360e900d292113e4a6d4
Joel <j.jagerskogh@gmail.com>
2022-11-09 14:46:20 +0100

    Lägger till komentarer till databas.sql
    (VS Code tror att det är Microsofts SQL och inte MYSQL, vilket leder till att den inte kan hjälpa så mycket när man skriver databas)

--- databas.sql
+++ databas.sql
@@ -1,3 +1,4 @@
+/*  Skapar tabellen users och gör så att Email måste vara uniqe for the user*/
 CREATE TABLE Users (
     ID int NOT NULL PRIMARY KEY AUTO_INCREMENT,
     FirstName varchar(30) NOT NULL,
@@ -5,6 +6,7 @@ CREATE TABLE Users (
     pwd varchar(255),
 );
 
+/*  Skapar tabellen msg kopplar ihop så att userID måste vara ett värde som finns i columnen ID i tabellen users*/
 CREATE TABLE Msg (
     ID int NOT NULL PRIMARY KEY AUTO_INCREMENT,
     Content varchar(8000) NOT NULL,
@@ -14,9 +16,10 @@ CREATE TABLE Msg (
 );
 
 
-
+/* Lägger till en user som heter Joel, har en mail och ett lösenord */
 INSERT INTO users (Email, FirstName, pwd)
 VALUES ('hej@hej.com', 'Joel', 'Passwords123')
 
+/* Lägger till ett meddelande med datum och vilken användare som skrev det; user med ID: 1*/
 INSERT INTO msg (Content, Datum, userID)
 VALUES ('Hej, här kommer ett meddelande!', '2022-11-09 10:02:16', 1);
