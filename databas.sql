d8d4b23f9c5a3a578b706754ebc9f2231503d241
Joel <j.jagerskogh@gmail.com>
2022-11-09 10:29:07 +0100

    Skapar kod för att skapa databas
    I databas.sql finns kod för att skap tabeller med en rad var
    
    I includes finns två funktioner som lägger till ett meddelande respektive en user med hjälp av argument som skickas till dem.

---
+++ databas.sql
@@ -0,0 +1,22 @@
+CREATE TABLE Users (
+    ID int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    FirstName varchar(30) NOT NULL,
+    Email varchar(100) UNIQE,
+    pwd varchar(255),
+);
+
+CREATE TABLE Msg (
+    ID int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    Content varchar(8000) NOT NULL,
+    userID int NOT NULL,
+    Datum datetime,
+    CONSTRAINT fk_Users FOREIGN KEY (userID) REFERENCES Users(ID)
+);
+
+
+
+INSERT INTO users (Email, FirstName, pwd)
+VALUES ('hej@hej.com', 'Joel', 'Passwords123')
+
+INSERT INTO msg (Content, Datum, userID)
+VALUES ('Hej, här kommer ett meddelande!', '2022-11-09 10:02:16', 1);
--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -9,4 +9,25 @@ $dbName = "testt";
 //Creates connection to the MySQL database
 $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
 
+
+function addUser($name, $mail, $pwd) {
+    // prepare and bind och gör så att SQL injections inte kan ske.
+    $stmt = $conn->prepare("INSERT INTO users (Email, FirstName, pwd) VALUES (?, ?, ?)");
+    $stmt->bind_param("sss", $name, $mail, $pwd);
+
+    $stmt->execute();
+    //Felhantering behövs (om det inte gick att skapa)
+}
+
+function addMsg($Content, $Datum, $userID) {
+    // prepare and bind och gör så att SQL injections inte kan ske.
+    $stmt = $conn->prepare("INSERT INTO msg (Content, Datum, userID) VALUES (?, ?, ?)");
+    $stmt->bind_param("ssi", $Content, $Datum, $userID);
+
+    $stmt->execute();
+    //Felhantering behövs (om det inte gick att skapa)
+}
+
+
+
 ?>
