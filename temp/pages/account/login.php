5071ec81abd2529e4a0f6ba79f30e81b04b25e4d
ErikasJanusauskas <erikas.janusauskas@elev.ga.ntig.se>
2023-01-16 23:16:15 +0100

    checkpoint (work in progress)
    Log in & Sign up updated design. Next step: merge new files with old.

---
+++ temp/pages/account/login.php
@@ -0,0 +1,66 @@
+<?php
+    //  login.php
+    //  If the user is not logged in, he/she will be redirected to this page.
+    //  The page will show a login form and post the data back to this file
+    //  If we get posted data, we check if the user exists and if so updates
+    //  the session with the user id to keep the user logged in.
+
+        //Includes the connection to the database
+        include_once '../../../includes/dbh.func/dbh.all.php';
+
+
+    //check if this is a login request
+    if(isset($_POST['email']) AND isset($_POST['pwd'])) {
+        // If so, check if the user exists in the database and returns ID
+        $id = loginValidation($_POST['email'], $_POST['pwd']);
+
+        // If the user didn't exist the function will return -1
+        if($id != -1) {
+            // if the user exists, we have now successfully logged in.
+            // We store the id in the session, to let the user contiune to be logged in
+            session_start(); // start the PHP_session function 
+            $_SESSION['userID'] = $id;
+
+            header("location:/NTI-website/pages/forum/forum.php");
+        } else {
+            echo "<p id='fail-msg'>Invalid username or password, try again</p>";
+        }
+    }
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <!-- style link  -->
+    <link rel="stylesheet" href="../../../style_v2/pages/account/account.css">
+    <link rel="stylesheet" href="../../../style_v2/main.css">
+
+    <title>Log in</title>
+</head>
+<body>
+    
+<section>
+
+    <h1>Welcome, consider logging in.</h1>
+
+        <form name="login" method="post" action="./login.php">
+
+            <input placeholder="Email" type="text" name="email">
+
+            <input placeholder="Password" type="password" name="pwd">
+
+            <button type="submit" id="submit">Log in</button>
+
+        </form>
+
+        <p>Dont have any account? No problem, <a href="sign-up.php">create one here!</a></p>
+
+</section>
+
+</body>
+</html>
---
+++ temp/pages/forum/forum_v2.php
@@ -0,0 +1,169 @@
+<?php 
+
+    //Includes functions related to the db
+    include_once '../../../includes/dbh.func/dbh.all.php';
+    include_once '../../../includes/loginCheck.php';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- style links -->
+    <link rel="stylesheet" href="../../../style_v2/main.css">
+    <link rel="stylesheet" href="../../../style_v2/common.css">
+
+    <link rel="stylesheet" href="../../../style_v2/pages/forum/forum.css">
+
+    <title>temp: Forum</title>
+</head>
+<body>
+
+    <header class="FP1">
+        <!-- Navigation bar -->
+        <nav>
+            <!-- Navbar button -->
+            <a href="">
+                <button class="navButton">Home</button>
+            </a>
+            <!-- Navbar button -->
+            <a href="">
+                <button class="navButton">News</button>
+            </a>
+            <!-- Navbar button -->
+            <a href="forum_v2.php">
+                <button class="navButton">Forum</button>
+            </a>
+            <!-- Navbar button -->
+            <a href="">
+                <button class="navButton">Schedule</button>
+            </a>
+            <!-- Navbar button -->
+            <a href="">
+                <button class="navButton">Memes</button>
+            </a>
+            <!-- Navbar button -->
+            <a href="">
+                <button class="navButton">Games</button>
+            </a>
+            <!-- Navbar button -->
+            <a href="">
+                <button class="navButton">Account</button>
+            </a>
+        </nav>
+
+
+        <!-- searchbar -->
+        <form class="searchbar" action="">
+            
+            <!-- ( "Searchfield where user are able to type" ) -->
+            <input class="searchfield" placeholder="Search . . ." type="text">
+
+            <!-- ( "Search Button" ) -->
+            <button class="searchButton">
+                <img class="icon" src="" alt="">
+            </button>
+        </form>
+
+
+        <!-- "ask a question" button -->
+        <button class="buttonType1">Ask a question</button>
+    </header>
+
+
+    <!-- corses -->
+    <div class="corses FP3">
+        <p>Courses</p>
+
+        <!-- Course Button -->
+        <button class="buttonType2">
+            <!-- Course name -->
+            <p>course 1</p> 
+            <!-- Course color -->
+            <div class="courseMark"></div>
+        </button>
+
+         <!-- Course Button -->
+         <button class="buttonType2">
+            <!-- Course name -->
+            <p>course 1</p> 
+            <!-- Course color -->
+            <div class="courseMark"></div>
+        </button>
+
+         <!-- Course Button -->
+         <button class="buttonType2">
+            <!-- Course name -->
+            <p>course 1</p> 
+            <!-- Course color -->
+            <div class="courseMark"></div>
+        </button>
+
+    </div>
+
+
+    <!-- Content feed -->
+    <section class="contentFeed FP2">
+
+
+        <?php 
+
+        $questions = getQuestions();
+
+        //  Amount of cards is the amount of cards that will be displayed
+        for($current = 0; $current < sizeof($questions); $current++){
+        
+            //  Gets the course we are in
+            $course = getCourseByID($questions[$current][1]);
+            $user = getUserFromId($questions[$current][2]);
+        ?>
+
+
+        <a href="forumQuestion.php">
+            <div class="forumCard">
+                <!-- Corse pill -->
+                <div class="pill QCP1">
+                    <p><?php echo $course[1] ?></p>
+                </div>
+
+                <!-- Vote -->
+                <button class="pill QCP2">
+                    <?php echo $questions[$current][6] ?>
+                    <!-- <img class="icon" src="" alt=""> -->
+                </button>
+
+                <!-- Card information -->
+                <div class="pill QCP3">
+                    <!-- Username -->
+                    <p><?php echo $user[1]; ?></p>
+
+                    <!-- Date -->
+                    <p><?php echo $questions[$current][5]; ?></p>
+                </div>
+
+                <!-- Card title/question -->
+                <p class="RegularText QCP4"><?php echo $questions[$current][3] ?></p>
+            </div>
+        </a>
+
+
+        <?php
+
+            }
+
+            // postQuestion(1, 1, "fråga", "dfvbskjvbsjbv", "2023-01-13 03:04:11", 0);
+
+        ?>
+
+
+    </section>
+    
+
+    <footer class="FP4"></footer>
+
+</body>
+</html>
