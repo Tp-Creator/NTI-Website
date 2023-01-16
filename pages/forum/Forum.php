fad6f1550abedd29f2c085d0548a8c52185fb875
ErikasJanusauskas <erikas.janusauskas@elev.ga.ntig.se>
2023-01-17 00:33:18 +0100

    Brain not braining fix

--- pages/forum/Forum.php
+++ pages/forum/Forum.php
@@ -14,7 +14,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <!-- style links -->
-    <link rel="stylesheet" href="../../style/main.css">
+    <link rel="stylesheet" href="../../style/main.css".css">
     <link rel="stylesheet" href="../../style/common.css">
     
     <link rel="stylesheet" href="../../style/pages/forum/forum.css">
