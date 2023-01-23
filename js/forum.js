d456958e9448fd8c27c3fe55ff3a30eaa2ededb6
Joel <j.jagerskogh@gmail.com>
2023-01-23 17:00:51 +0100

    slideUp and slideDown on the card animation!

--- js/forum.js
+++ js/forum.js
@@ -4,13 +4,20 @@
 
         //  När man klicka på knappen med id:t #askQuestion körs funktionen.
     $("#askQuestion").click(function(){
+            //  Vi kollar först om kortet redan syns eller inte, gör det det så tar vi bort det och tvärt om.
         if($("#askNewQuestionCard").is(":visible")){
-            $("#askNewQuestionCard").hide();
+                //  .slideUp(300) är en animation som gör att den försvinner på 300 millisekunder.
+            $("#askNewQuestionCard").slideUp(300);
+                //  Ändrar även texten på knappen så att det står ask a question.
             $("#askQuestion").text("Ask a question");
           } else {
-            $("#askNewQuestionCard").show();
+                //  Samma som föregående fast tvärt om
+            $("#askNewQuestionCard").slideDown(300);
             $("#askQuestion").text("Cancel");
           }
     });
  
+    
+
+
 });
