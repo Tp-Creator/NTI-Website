dc1023990d0b5a7b8824f36d752326dba1be06c2
PythonErmine <73660649+PythonErmine@users.noreply.github.com>
2023-01-26 15:29:30 +0100

    401 test 2

--- includes/loginCheck.php
+++ includes/loginCheck.php
@@ -7,7 +7,8 @@ session_start(); //start the PHP_session function
 
 if(!isset($_SESSION['userID']))
 {
-    http_response_code(404);
+    header("HTTP/1.1 401 Unauthorized");
+    // http_response_code(404);
     //header("location:../../pages/account/login.php");
     //header("location:/pages/account/login/login.php");
 }
