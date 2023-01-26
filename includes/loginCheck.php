f38838df5a217d8302399330d8c5d6a9594ecebb
PythonErmine <73660649+PythonErmine@users.noreply.github.com>
2023-01-26 15:33:04 +0100

    401
    med hjälp av ChatGPT

--- includes/loginCheck.php
+++ includes/loginCheck.php
@@ -7,10 +7,8 @@ session_start(); //start the PHP_session function
 
 if(!isset($_SESSION['userID']))
 {
-    header("HTTP/1.1 401 Unauthorized");
-    // http_response_code(404);
-    //header("location:../../pages/account/login.php");
-    //header("location:/pages/account/login/login.php");
+    http_response_code(401);
+    header("location:/pages/account/login/login.php");
 }
 
 ?>
