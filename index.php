d3aea61519a881b1169eaa062ae84dba61c8b394
Joel <j.jagerskogh@gmail.com>
2023-02-09 17:44:16 +0100

    BreadCrumbs kinda works....
    Man måste dock ha 404 till index.php

--- index.php
+++ index.php
@@ -1,34 +1,111 @@
 <?php
-
 include_once('includes\dbh.inc.php');
 
-console_log($_SERVER);
 
 
-$pages = [
-    'home' => [
-        'file' => '403.html',
-        'title' => 'Home',
-    ],
-    'about' => [
-        'file' => 'about.php',
-        'title' => 'About',
-    ],
-    'login' => [
-        'file' => '/pages/account/login/login.php',
-        'title' => 'login',
-    ],
+$routes = [
+    "/forum" => "/pages/forum/main/forum.php",
+    '/login' => '/pages/account/login/login.php',
 ];
 
-$route = $_GET['route'] ?? 'home';
+// route('/', function () {
+//     echo "Home page";
+// });
 
-if (!isset($pages[$route])) {
-    http_response_code(404);
-    exit('Page not found');
+// route('/login', function () {
+//     echo "Login page";
+// });
+
+// function route(string $path, callable $callback){
+//     global $routes;
+//     $routes[$path] = $callback;
+// }
+
+run();
+
+function run(){
+    global $routes;
+    $uri = $_SERVER['REQUEST_URI'];    
+    foreach ($routes as $path => $url) {
+        if ($path === $uri) {
+            // console_log($url);
+            require __DIR__ . $url;
+            // $callback();
+        } #continue;     callback();
+    }
 }
 
-$page = $pages[$route];
 
-require __DIR__ . '/' . $page['file'];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console_log($_SERVER);
+
+
+// $pages = [
+//     'home' => [
+//         'file' => '403.html',
+//         'title' => 'Home',
+//     ],
+//     'about' => [
+//         'file' => 'about.php',
+//         'title' => 'About',
+//     ],
+//     'login' => [
+//         'file' => '/pages/account/login/login.php',
+//         'title' => 'login',
+//     ],
+// ];
+
+// $route = $_GET['route'] ?? 'home';
+
+// if (!isset($pages[$route])) {
+//     http_response_code(404);
+//     exit('Page not found');
+// }
+
+// $page = $pages[$route];
+
+// require __DIR__ . '/' . $page['file'];
+
+
+
+
+
+
+
+
+
+
 
 ?>
