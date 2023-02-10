8234076ac26bfe0c98c77265ca606a85e97a50d1
PythonErmine <73660649+PythonErmine@users.noreply.github.com>
2023-02-10 12:07:52 +0100

    Name changes

--- routing.php
+++ routing.php
@@ -3,8 +3,9 @@ include_once('includes\dbh.inc.php');
 
 
 $routes = [
+    "/"      => "/pages/index.html",
     "/forum" => "/pages/forum/main/forum.php",
-    '/login' => '/pages/account/login/login.php',
+    "/login" => "/pages/account/login/login.php",
 ];
 console_log($_SERVER);
 
@@ -16,6 +17,7 @@ function run() {
     $uri = $_SERVER['REQUEST_URI'];    
     foreach ($routes as $path => $url) {
         if ($path === $uri) {
+            console_log($url);
             require __DIR__ . $url;
             return;
         }
