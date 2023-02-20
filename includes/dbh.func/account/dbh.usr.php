4759744c68a274676226a5fcc420fa5f5622b4bf
Joel <j.jagerskogh@gmail.com>
2023-02-20 16:14:20 +0100

    Tog bort en gammal funktion

--- includes/dbh.func/account/dbh.usr.php
+++ includes/dbh.func/account/dbh.usr.php
@@ -56,16 +56,16 @@ function addUser($name, $mail, $pwd) {
 //  getUsernameFromId($id)
 //  this function takes one argument: the Id of a user
 //  And returns the username of the specific user
-function getUserFromId($id) {
-    global $conn;
+// function getUserFromId($id) {
+//     global $conn;
     
-    $stmt = $conn->prepare("SELECT * FROM users WHERE userID = ?;");
-    $stmt->bind_param("s", $id);
-    $stmt->execute();
-    $result = $stmt->get_result();
-    return $result->fetch_all()[0];
-    // return $result;
-}
+//     $stmt = $conn->prepare("SELECT * FROM users WHERE userID = ?;");
+//     $stmt->bind_param("s", $id);
+//     $stmt->execute();
+//     $result = $stmt->get_result();
+//     return $result->fetch_all()[0];
+//     // return $result;
+// }
 
 
 //  loginValidation($email, $pwd)
