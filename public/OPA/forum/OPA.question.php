876c3dcbfbc4d962eff58c638135422116043a02
Joel <j.jagerskogh@gmail.com>
2023-03-06 10:06:46 +0100

    small changes

--- public/OPA/forum/OPA.question.php
+++ public/OPA/forum/OPA.question.php
@@ -1,11 +1,10 @@
 <?php 
-// return;
-// echo "hej";
+
 set_include_path($_SERVER['DOCUMENT_ROOT']);
 
 include_once 'includes/dbh.func/forum/dbh.forum.php';
+include_once('includes/dbh.func/general/dbh.inc.php');
 
-// console_log("hej");
 
 session_start(); //start the PHP_session function 
 
@@ -13,6 +12,7 @@ session_start(); //start the PHP_session function
 $questionID = htmlspecialchars($_POST["questionID"], ENT_QUOTES, 'UTF-8');
 $userID = htmlspecialchars($_SESSION['userID'], ENT_QUOTES, 'UTF-8');
 $content = htmlspecialchars($_POST['newAnswerContent'], ENT_QUOTES, 'UTF-8');
+
 
 postAnswer($questionID, $userID, $content, floor(microtime(true) * 1000));
 
