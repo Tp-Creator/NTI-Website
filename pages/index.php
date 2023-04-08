fcdb109459e1934a410ac58dd0c28930076007b6
ErikasJanusauskas <erikas.janusauskas@elev.ga.ntig.se>
2023-04-08 11:44:28 +0200

    New meta tag.
    The meta tag update is supposed to set a theme color for the website so the web browser knows what color it should adapt to.

--- pages/index.php
+++ pages/index.php
@@ -12,19 +12,20 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>We're GradeLess</title>
+    <meta name="theme-color" content="#101014">
     <!-- random style links -->
     <link rel="stylesheet" href="/public/style/mainStyle.css">
     <link rel="stylesheet" href="/public/style/commonStyle.css">
     <link rel="stylesheet" href="/public/style/indexStyle.css">
 
-        <!-- Navigationbar  -->
-        <nav>
-        <?php echo drawNavbar() ?>
-        </nav>
-
+    <title>We're GradeLess</title>
 </head>
 <body>
+
+    <!-- Navigationbar  -->
+    <nav>
+        <?php echo drawNavbar() ?>
+    </nav>
 
     <div class="infoSection">
         <img class="infoImg" src="../public/style/includes/img/Img_placeholder.svg" alt="">
--- pages/memes/memes.php
+++ pages/memes/memes.php
@@ -12,6 +12,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="theme-color" content="#101014">
 
     <link rel="stylesheet" href="/public/style/mainStyle.css">
     <link rel="stylesheet" href="/public/style/commonStyle.css">
