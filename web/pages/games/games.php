7563d0e03905622b38eb8ab8b49d3b5b6be4d527
Joel <j.jagerskogh@gmail.com>
2023-04-19 09:34:31 +0200

    bruh

--- web/pages/games/games.php
+++ web/pages/games/games.php
@@ -28,12 +28,12 @@
 
     <div id="contentFeed">
         <div class="gCard">
-            <img class="gcIMG" src="/public/style/includes/img/placeholder_16_9.svg" alt="">
+            <img class="gcIMG" src="/public/img/gamesThumbnail/2Tetris_Joel_J.png" alt="">
             <div class="gCardFooter">
-                <p class="gcTitle">Title</p>
-                <p class="gcDesc">publisher</p>
+                <p class="gcTitle">2Tetris</p>
+                <p class="gcDesc">Joel J - TE21</p>
 
-                <a class="gcBtn" href="">Play</a>
+                <a class="gcBtn" href="/games/2Tetris">Play</a>
             </div>
         </div>
 
