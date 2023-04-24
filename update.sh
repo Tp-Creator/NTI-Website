9d159a20de07bc0f1e4e692581fc95517ab46568
PythonErmine <73660649+PythonErmine@users.noreply.github.com>
2023-04-24 12:44:29 +0200

    This is a commit

--- update.sh
+++ update.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
-git pull
-systemctl restart httpd
+seconds_per_hour=3600
+while true
+do
+    git pull
+    systemctl restart httpd
 
-# Run python files
-python python/schoolfood.py
+    # Run python files
+    python python/schoolfood.py
+
+
+    # Wait aproximatly 24 hours before running the loop again
+    seconds_after_midnight=$(($(date -u +%s) % (seconds_per_hour*24) + (seconds_per_hour*2)))
+    sleep $secondsPerHour*25 - $seconds_after_midnight
+done
